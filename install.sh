40a94edc09a49d022619be1bb5be12e4ab243fcd
Jan Peter Koenig <j.koenig@pons.de>
2020-02-03 14:48:36 +0100

    typo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ BUILD_DIR="/tmp/cm"
 source /etc/os-release
 
 if [ "$ID" == "debian" ]; then
-  apt-get install -y build-essentail libyaml-cpp-dev libboost-all-dev cmake git || exit 1
+  apt-get install -y build-essential libyaml-cpp-dev libboost-all-dev cmake git || exit 1
   git clone "$REPOSITORY" "$BUILD_DIR" || exit 1
   cd "$BUILD_DIR" || exit 1
   cmake -S . -B "$BUILD_DIR/build" || exit 1
